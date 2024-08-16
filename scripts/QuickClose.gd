6bbdcb8b860595907f514ae6afc4834d29082622
Chris Lahey <clahey@clahey.net>
2024-08-16 19:20:37 -0400

    Remove call which was causing errors.

--- scripts/QuickClose.gd
+++ scripts/QuickClose.gd
@@ -25,14 +25,6 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"ui_cancel"):
 		get_tree().quit() # Quits the game
 
-	if event.is_action_pressed(&"change_mouse_input"):
-		match Input.get_mouse_mode():
-			Input.MOUSE_MODE_CAPTURED:
-				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			Input.MOUSE_MODE_VISIBLE:
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
 # Capture mouse if clicked on the game, needed for HTML5
 # Called when an InputEvent hasn't been consumed by _input() or any GUI item
 #func _unhandled_input(event: InputEvent) -> void:
