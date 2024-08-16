b8026de518fdd8f28d66077afbb5f7d515c0896b
Chris Lahey <clahey@clahey.net>
2024-08-16 19:17:08 -0400

    Make MouseControllable support RigidBody3d parents.

--- scripts/mouse_controllable.gd
+++ scripts/mouse_controllable.gd
@@ -3,27 +3,41 @@ extends Node
 
 var grabbed = false
 var parent: PhysicsBody2D
+var parentCharacter: CharacterBody2D
+var parentRigid: RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	parent = get_parent() as PhysicsBody2D
+	parentCharacter = parent as CharacterBody2D
+	parentRigid = parent as RigidBody2D
+	parent.input_pickable = true
 	parent.input_event.connect(_input_event)
 	set_process_input(false)
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if (event is InputEventMouseButton):
 		if (event.is_pressed()):
-			parent.set_physics_process(false)
 			grabbed = true
-			parent.velocity.y = 0
+			if parentRigid:
+				parentRigid.freeze = true
+			if parentCharacter:
+				parentCharacter.set_physics_process(false)
+			grabbed = true
 			set_process_input(true)
 
 func _input(event: InputEvent):
 	if grabbed:
 		if (event is InputEventMouseButton):
 			if (event.is_released()):
-				parent.set_physics_process(true)
+				if parentRigid:
+					parentRigid.freeze = false
+				if parentCharacter:
+					parentCharacter.set_physics_process(true)
 				grabbed = false
 				set_process_input(false)
 		elif (event is InputEventMouseMotion):
-			parent.position += event.relative
+			if parentRigid:
+				parentRigid.global_position += event.relative
+			if parentCharacter:
+				parentCharacter.position += event.relative
