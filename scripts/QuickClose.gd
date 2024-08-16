eaa09ea88aa5b334d6e2e5962e3c6321a11414ca
Mige <mige.kirin@gmail.com>
2024-08-16 18:15:28 -0400

    work on scale stuff

--- scripts/QuickClose.gd
+++ scripts/QuickClose.gd
@@ -35,7 +35,7 @@ func _input(event: InputEvent) -> void:
 
 # Capture mouse if clicked on the game, needed for HTML5
 # Called when an InputEvent hasn't been consumed by _input() or any GUI item
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-			if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-				Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton:
+			#if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+				#Input.mouse_mode = Input.MOUSE_MODE_CONFINED
---
+++ scripts/arm.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+
+func _on_rigid_body_2d_2_body_entered(body: Node) -> void:
+	print("Mass: %s kg" % body.mass)
