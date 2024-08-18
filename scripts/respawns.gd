0dae2d626140ce7a91d0da03f6294ecc52cdbcbb
Chris Lahey <clahey@clahey.net>
2024-08-17 22:12:26 -0400

    Handle no objectController without crashing.

--- scripts/respawns.gd
+++ scripts/respawns.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 	parent = get_parent() as Node2D
 
 func _physics_process(_delta: float) -> void:
-	if parent:
+	if parent and objectController:
 		if parent.position.y > floorLevel:
 			objectController.place_object_in_scene(parent)
